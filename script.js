7604917face0287438d7f94b67f645f7f22e736b
Isabell Hua <hua.isabelle@gmail.com>
2017-04-23 18:55:32 -0700

    redirecting

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    // set the initial page to calculator
+    history.pushState(null, "", "/calculator");
+
+    function handlePageChange() {
+      // get the current page from the url
+        var currentLocation = document.location.pathname.split("/")[1];
+      // only display that page
+        var pages = $("#pages").children();
+        var foundMatch = false;
+        for (var i=0;i<pages.length;i++) {
+        if (a.id != currentLocation) {
+          a.style.display = "none";
+        } else {
+          a.style.display = "";
+          foundMatch = true;
+        }
+      }
+      if (!foundMatch) {
+        $("#404").style("display","");
+      }
+    };
+    document.addEventListener("locationchange", handlePageChange);
     var number = "";
     var newnumber = "";
     var operator = "";
@@ -47,28 +69,7 @@ $(document).ready(function () {
         newnumber = "";
     });
     
-    // set the initial page to calculator
-    history.pushState(null, "", "/calculator");
-
-    function handlePageChange() {
-      // get the current page from the url
-        var currentLocation = document.location.pathname.split("/")[1];
-      // only display that page
-        var pages = $("#pages").children();
-        var foundMatch = false;
-        for (var i=0;i<pages.length;i++) {
-        if (a.id != currentLocation) {
-          a.style.display = "none";
-        } else {
-          a.style.display = "";
-          foundMatch = true;
-        }
-      }
-      if (!foundMatch) {
-        $("#404").style("display","");
-      }
-    };
-    document.addEventListener("locationchange", handlePageChange);
+    
 });
 
 
