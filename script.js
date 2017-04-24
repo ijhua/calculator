a9d880beb3b4865f5519d425b085b4448893161a
Isabell Hua <hua.isabelle@gmail.com>
2017-04-23 19:19:21 -0700

    testing change button

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
     });
     
     $("#change").on("click",function(){
-       history.pushState(null, "", "/404");
+       history.pushState(null, "", "/calculator/404");
         document.dispatchEvent(new Event("locationchange"));
     });
 });
