629433284fb93e77bb9af351a7e1e4d710e07797
Student User <Student User>
2017-05-05 11:51:36 -0700

    change urls on click

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
     $("#to-calc").on("click",function(){changePage("/calculator/calculator")});
     $("#to-foo").on("click",function(){changePage("/calculator/foo")});
     
-    changePage("/calculator/");
+    changePage("/calculator");
 });
 
 function changePage(name) {
